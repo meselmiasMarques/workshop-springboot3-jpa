2ef50cc15be2a64873d38e3257638b160b9e624d
Meselmias Marques <meselmias.marques.lima@gmail.com>
2023-10-24 21:58:26 -0600

    add atributo order part 1

---
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -0,0 +1,4 @@
+package com.educandoweb.course.entities;
+
+public class Product {
+}
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,10 @@
 package com.educandoweb.course.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -21,10 +19,14 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
 	}
+
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -62,6 +64,10 @@ public class User implements Serializable {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	@Override
 	public int hashCode() {
