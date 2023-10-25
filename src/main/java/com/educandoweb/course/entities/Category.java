0dd341419057995c0022bd901a6941b4b1800b2c
Meselmias Marques <meselmias.marques.lima@gmail.com>
2023-10-25 16:45:20 -0600

    criado implementação de categorias e produtos]

---
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -0,0 +1,68 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
+
+    public Set<Product> getProducts() {
+		return products;
+	}
+    
+    public Category() {
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+	
+
+	
+}
