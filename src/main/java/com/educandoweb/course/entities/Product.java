f3a834f243d763939613f94b42558668941b87a0
Meselmias Marques <meselmias.marques.lima@gmail.com>
2023-10-26 15:50:10 -0600

    product orderitem

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -18,12 +20,16 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
+    
     @ManyToMany
     @JoinTable(name = "tb_products_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+    
     public Product() {
 
     }
@@ -79,6 +85,14 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+    @JsonIgnore
+    public Set<Order> getOrders(){
+    	Set<Order> set = new HashSet<>();
+    	for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+    	return set;
     }
 
     @Override
